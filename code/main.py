1e162494e2cfa0e5bfea62e6a3b162df3f2bc4be
Nick Puljic <nap2152@columbia.edu>
2018-02-26 19:22:53 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World from AutoIntern"
+    return "Hello World from AutoIntern "
 
 if __name__ == '__main__':
     app.run(debug=True)
